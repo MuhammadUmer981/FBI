7cffd62f831e9cc15f75fa27e028e8398ec86ad9
qadeerinstant77 <abdul.qadeer@instantsolutionslab.site>
2024-08-26 14:03:37 +0500

    push

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 const session = require('express-session');
-const cors = require('cors');
-const redisClient = require('redis').createClient();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.use(session({
     resave: false,
